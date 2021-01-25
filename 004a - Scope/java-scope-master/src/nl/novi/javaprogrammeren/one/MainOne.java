949ee7f9d4a4f763f39dea129136e5c6ec9a7ba9
=Rob Franke <robwfranke@gmail.com>
2021-01-25 09:28:14 +0100

    "begin"

---
+++ 004a - Scope/java-scope-master/src/nl/novi/javaprogrammeren/one/MainOne.java
@@ -0,0 +1,12 @@
+package nl.novi.javaprogrammeren.one;
+
+public class MainOne {
+
+    public static void main(String[] args) {
+
+        Person sjaak = new Person("Nick");
+        sjaak.setAge(23);
+        System.out.println(sjaak.getAge());
+
+    }
+}
---
+++ 004a - Scope/java-scope-master/src/nl/novi/javaprogrammeren/one/Person.java
@@ -0,0 +1,28 @@
+//package nl.novi.javaprogrammeren.one;
+//
+//public class Person {
+//
+//    private String name;
+//
+//    public Person(String name) {
+//        this.name = name;
+//    }git commit "begin"
+//
+//    public void setAge(int age) {
+//        age = age;
+//    }
+//}
+package nl.novi.javaprogrammeren.one;
+
+public class Person {
+
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        age = age;
+    }
+}
