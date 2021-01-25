baf9c8488b58289f40b54bf896f761423dacf918
=Rob Franke <robwfranke@gmail.com>
2021-01-25 12:55:37 +0100

    opgave 4c, one and two and 3b

--- 004c - Keywords_and_static_and_overloading/java-keywords-static-overloading-master/src/nl/novi/javaprogrammeren/three/Beer.java
+++ 004c - Keywords_and_static_and_overloading/java-keywords-static-overloading-master/src/nl/novi/javaprogrammeren/three/Beer.java
@@ -2,8 +2,24 @@ package nl.novi.javaprogrammeren.three;
 
 public class Beer {
     private String brand;
+    static int numberOfBeers=0;
 
     public Beer(String brand) {
         this.brand = brand;
+        numberOfBeers++;
+        System.out.println("Dit is biertje nummer:" + numberOfBeers);
     }
 }
+
+
+
+
+//package nl.novi.javaprogrammeren.three;
+//
+//public class Beer {
+//    private String brand;
+//
+//    public Beer(String brand) {
+//        this.brand = brand;
+//    }
+//}
--- 004c - Keywords_and_static_and_overloading/java-keywords-static-overloading-master/src/nl/novi/javaprogrammeren/three/MainThree.java
+++ 004c - Keywords_and_static_and_overloading/java-keywords-static-overloading-master/src/nl/novi/javaprogrammeren/three/MainThree.java
@@ -8,5 +8,28 @@ public class MainThree {
 
 
 
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+//package nl.novi.javaprogrammeren.three;
+//
+//public class MainThree {
+//    public static void main(String[] args) {
+//        Beer amstelBier = new Beer("Amstel");
+//        Beer brandBier = new Beer("Brand");
+//        Beer heinekenBier = new Beer("Heineken");
+//
+//
+//
+//    }
+//}
