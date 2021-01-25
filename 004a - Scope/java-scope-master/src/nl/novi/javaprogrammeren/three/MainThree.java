8522c797de686eaf9eebdd41a0ecfc7d9d63298a
=Rob Franke <robwfranke@gmail.com>
2021-01-25 10:34:23 +0100

    opgave 3

--- 004a - Scope/java-scope-master/src/nl/novi/javaprogrammeren/three/MainThree.java
+++ 004a - Scope/java-scope-master/src/nl/novi/javaprogrammeren/three/MainThree.java
@@ -4,5 +4,12 @@ public class MainThree {
     public static void main(String[] args) {
         Phone phone = new Phone();
         phone.setPhoneNumber("612345678");
+        System.out.println(phone.getPhoneNumber());
+        System.out.println(phone.getInternationalPhoneNumber());
+
     }
 }
+
+
+
+// getter methode uit klasse Phone
